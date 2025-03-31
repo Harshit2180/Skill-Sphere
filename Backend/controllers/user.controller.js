ddaf3b6469f45a9f7c42f7b8cb9d268b1ca17e44
Harshit2180 <chopraharshit2180@gmail.com>
2025-03-31 13:45:31 +0530

    Create login route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -46,7 +46,7 @@ export const login = async (req, res) => {
         const { email, password } = req.body;
         if (!email || !password) {
             return res.status(400).json({
-                success: true,
+                success: false,
                 message: "Something is missing"
             })
         }
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 
@@ -11,7 +13,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("api/v1/user", userRoute)
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/v1/user", userRoute)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
