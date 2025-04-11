b74f2f276c3ceab4dcfc2ec4d8dc5566bee04894
Harshit2180 <chopraharshit2180@gmail.com>
2025-04-11 13:15:04 +0530

    Design course detail page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -44,7 +44,7 @@ const appRouter = createBrowserRouter([
         element: <Profile />
       },
       {
-        path: "course-detail",
+        path: "course-detail/:courseId",
         element: <CourseDetail />
       },
 
--- Frontend/src/pages/student/CourseDetail.jsx
+++ Frontend/src/pages/student/CourseDetail.jsx
@@ -1,7 +1,14 @@
-import { BadgeInfo } from 'lucide-react'
+import BuyCourseButton from '@/components/BuyCourseButton'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import { BadgeInfo, Lock, PlayCircle } from 'lucide-react'
 import React from 'react'
 
 const CourseDetail = () => {
+
+    const purchasedCourse = true;
+
     return (
         <div className='mt-20 space-y-5'>
             <div className='bg-[#2D2F31] text-white'>
@@ -20,6 +27,43 @@ const CourseDetail = () => {
                 <div className='w-full lg:w-1/2 space-y-2'>
                     <h1 className='font-bold text-xl md:text-2xl'>Description</h1>
                     <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, expedita! Ex dignissimos libero repudiandae fuga sapiente aut quae quod consectetur nulla reiciendis placeat tempore iste corporis, debitis earum voluptate blanditiis!</p>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Course Content</CardTitle>
+                            <CardDescription>10 Lectures</CardDescription>
+                        </CardHeader>
+                        <CardContent className='space-y-3'>
+                            {
+                                [1, 2, 3].map((lecture, idx) => (
+                                    <div key={idx} className='flex items-center gap-3 text-sm'>
+                                        <span>
+                                            {
+                                                true ? (<PlayCircle size={14} />) : <Lock size={14} />
+                                            }
+                                        </span>
+                                        <p>Lecture Title</p>
+                                    </div>
+                                ))
+                            }
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className='w-full lg:w-1/3'>
+                    <Card>
+                        <CardContent className='p-4 flex flex-col'>
+                            <div className='w-full aspect-video mb-4'>
+                                React player video here
+                            </div>
+                            <h1>Lecture Title</h1>
+                            <Separator className='my-2' />
+                            <h1 className='text-lg md:text-xl font-semibold'>Cours Price</h1>
+                        </CardContent>
+                        <CardFooter className='flex justify-center p-4'>
+                            {
+                                purchasedCourse ? (<Button className='w-full'>Continue Course</Button>) : (<BuyCourseButton />)
+                            }
+                        </CardFooter>
+                    </Card>
                 </div>
             </div>
         </div>
