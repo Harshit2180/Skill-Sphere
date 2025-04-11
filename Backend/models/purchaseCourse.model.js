e52f8c419b7313adda81ed9a7783edae5e4f9a00
Harshit2180 <chopraharshit2180@gmail.com>
2025-04-11 13:14:09 +0530

    Create course purchase controller and model

---
+++ Backend/models/purchaseCourse.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const coursePurchaseSchema = new mongoose.Schema({
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    },
+    paymentId: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+export const CoursePurchase = mongoose.model('CoursePurchase', coursePurchaseSchema)
