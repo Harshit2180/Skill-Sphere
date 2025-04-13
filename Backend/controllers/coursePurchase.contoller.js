220120612486f32d5b5fbcf2c2bff9a542c5504d
Harshit2180 <chopraharshit2180@gmail.com>
2025-04-13 16:12:39 +0530

    Create course progress model, controller and route

--- Backend/controllers/coursePurchase.contoller.js
+++ Backend/controllers/coursePurchase.contoller.js
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 import { Course } from "../models/course.model.js";
 import { CoursePurchase } from "../models/coursePurchase.model.js";
+import { Lecture } from "../models/lecture.model.js"
+import { User } from "../models/user.model.js"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -140,3 +142,44 @@ export const stripeWebhook = async (req, res) => {
     }
     res.status(200).send();
 };
+
+export const getCouruseDetailWithPurchaseStatus = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        const userId = req.id;
+
+        const course = await Course.findById(courseId).populate({ path: "creator" }).populate({ path: "lectures" })
+
+        if (!course) {
+            return res.status(404).json({
+                message: "Course not found"
+            })
+        }
+
+        return res.status(200).json({
+            course,
+            purchased: !!purchased
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getAllPurchasedCourse = async (_, res) => {
+    try {
+        const purchasedCourse = await CoursePurchase.find({ status: "completed" }).populate("courseId");
+        if (!purchasedCourse) {
+            return res.status(404).json({
+                purchasedCourse: []
+            })
+        }
+
+        return res.status(200).json({
+            purchasedCourse
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
